42b6b0240596adb5cc683bf40b0444ad20d58ad2
Karishma <knimgao@ncsu.edu>
2015-10-02 17:33:38 -0400

    Adding java project with error to make build fail

---
+++ trilith.devops.build.maven.java/src/main/java/trilith/devops/build/maven/java/App.java
@@ -0,0 +1,14 @@
+package trilith.devops.build.maven.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Missing ";" to throw an error and make the build fail !
+        System.out.println( "Hello World!" )
+    }
+}
